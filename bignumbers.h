a4e1dbed3775c677f805497ebae52bd5343d2688
Anton Dmitriev <tosha.dmitriefff@yandex.ru>
2024-11-12 10:43:16 +0100

    feat(BigInteger): add constructors and stream support for input/output

--- bignumbers.h
+++ bignumbers.h
@@ -5,18 +5,21 @@
 #pragma once
 
 #include <string>
-#include <stdint.h>
+#include <cstdint>
+#include <stdexcept>
 
-#define SUPPORT_IFSTREAM 0
-#define SUPPORT_MORE_OPS 0
-#define SUPPORT_EVAL 0
+#define SUPPORT_IFSTREAM 1
+#define SUPPORT_MORE_OPS 1
+#define SUPPORT_EVAL 1
+
+/* BigInteger */
 
 class BigInteger
 {
 public:
     // constructors
     BigInteger();
-    BigInteger(int64_t n);
+    explicit BigInteger(int64_t n);
     explicit BigInteger(const std::string& str);
     // copy
     BigInteger(const BigInteger& other);
@@ -37,8 +40,84 @@ public:
     bool is_prime(size_t k) const;
 #endif
 private:
+    std::string value;
+    char sign;
 
+    // friends
+    friend std::ostream& operator<<(std::ostream& lhs, const BigInteger& rhs);
+    friend std::istream& operator>>(std::istream& lhs, BigInteger& rhs);
+
+    // assistants
+    void removeLeadingZeros();
 };
+
+/* Constructors */
+
+inline BigInteger::BigInteger() : value("0"), sign('+') {}
+
+inline BigInteger::BigInteger(int64_t n)
+{
+    value = std::to_string(std::abs(n));
+    sign = (n < 0) ? '-' : '+';
+}
+
+inline BigInteger::BigInteger(const std::string& str)
+{
+    if (str.empty())
+    {
+        throw std::invalid_argument("empty string");
+    }
+
+    size_t pos = 0;
+    if (str[0] == '-' || str[0] == '+')
+    {
+        sign = str[0];
+        pos = 1;
+    }
+    else
+    {
+        sign = '+';
+    }
+
+    value = str.substr(pos);
+
+    if (value.empty() || value.find_first_not_of("0123456789") != std::string::npos)
+    {
+        throw std::invalid_argument("invalid number format");
+    }
+
+    removeLeadingZeros();
+}
+
+/* Copy */
+
+inline BigInteger::BigInteger(const BigInteger& other) = default;
+
+inline BigInteger& BigInteger::operator=(const BigInteger& rhs) {
+    if (this != &rhs)
+    {
+        value = rhs.value;
+        sign = rhs.sign;
+    }
+    return *this;
+}
+
+/* Assistants */
+
+inline void BigInteger::removeLeadingZeros()
+{
+    size_t pos = value.find_first_not_of('0');
+
+    if (pos == std::string::npos)
+    {
+        value = "0";
+        sign = '+';
+    }
+    else
+    {
+        value.erase(0, pos);
+    }
+}
 
 inline BigInteger operator+(BigInteger lhs, const BigInteger& rhs);
 inline BigInteger operator-(BigInteger lhs, const BigInteger& rhs);
@@ -53,12 +132,58 @@ inline bool operator>(const BigInteger& lhs, const BigInteger& rhs);
 inline bool operator<=(const BigInteger& lhs, const BigInteger& rhs);
 inline bool operator>=(const BigInteger& lhs, const BigInteger& rhs);
 
-inline std::ostream& operator<<(std::ostream& lhs, const BigInteger& rhs);
+/* Friends */
+
+inline std::ostream& operator<<(std::ostream& lhs, const BigInteger& rhs)
+{
+    if (rhs.sign == '-')
+    {
+        lhs << '-';
+    }
+
+    lhs << rhs.value;
+
+    return lhs;
+}
 
 #if SUPPORT_IFSTREAM == 1
-inline std::istream& operator>>(std::istream& lhs, BigInteger& rhs);
+inline std::istream& operator>>(std::istream& lhs, BigInteger& rhs)
+{
+    std::string input;
+    lhs >> input;
+
+    if (input.empty())
+    {
+        lhs.setstate(std::ios::failbit);
+        return lhs;
+    }
+
+    size_t pos = 0;
+    if (input[0] == '-' || input[0] == '+')
+    {
+        rhs.sign = input[0];
+        pos = 1;
+    }
+    else
+    {
+        rhs.sign = '+';
+    }
+
+    rhs.value = input.substr(pos);
+    if (rhs.value.empty() || rhs.value.find_first_not_of("0123456789") != std::string::npos)
+    {
+        lhs.setstate(std::ios::failbit);
+        rhs = BigInteger(0);
+        return lhs;
+    }
+
+    rhs.removeLeadingZeros();
+
+    return lhs;
+}
 #endif
 
+/* BigRational */
 
 class BigRational
 {
--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
+#include <iostream>
+
 #include "bignumbers.h"
 
 int main()
 {
+    BigInteger num1(-12345);
+    std::cout << num1 << std::endl;
+
+    BigInteger num2("67890");
+    std::cout << num2 << std::endl;
+
+    BigInteger num3("123456789012345678901234567890");
+    std::cout << num3 << std::endl;
+
+    BigInteger num4("+0000078901234567890");
+    std::cout << num4 << std::endl;
+
+    BigInteger num5("-987654321098765432109876543210");
+    std::cout << num5 << std::endl;
+
+    BigInteger num6("0000123400");
+    std::cout << num6 << std::endl;
+
+    BigInteger num7("0");
+    std::cout << num7 << std::endl;
+
+    BigInteger num8("-0000000000");
+    std::cout << num8 << std::endl;
+
     return 0;
 }
